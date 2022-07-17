a83e02fc85f99c69e1544fe859df6acaa01590b8
Joanne <>
2022-07-17 23:56:28 +0800

    update script

--- test.sh
+++ test.sh
@@ -63,7 +63,7 @@ echo "end time:"
 echo $(date +"%Y-%m-%dT%T.%3N%z")
 # docker build -t bc/cert-issuer:1.0 .
 sudo sh -c "truncate -s 0 /var/lib/docker/containers/*/*-json.log"
-docker run -e ENV_NONCE=12 --name test1 -d bc/cert-issuer:1.0
+docker run -e ENV_NONCE=13 --rm --name test1 -d bc/cert-issuer:1.0
 # docker start test1
 docker logs test1 -f > log.txt
 docker rm --name test1
